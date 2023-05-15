9b6960b417a9046ca882a753abbbe5532093efcf
Thabish <kadertabish2@gmail.com>
2023-05-15 12:03:28 +0400

    puppeteer-installed
    Signed-off-by: Thabish <kadertabish2@gmail.com>

---
+++ app/scrape/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import puppeteer from "puppeteer";
+
+export async function GET(request: Request) {
+	const { searchParams } = new URL(request.url);
+	const url = searchParams.get("url");
+
+	if (!url) {
+		return NextResponse.json(
+			{ error: "URL parameter is required" },
+			{ status: 400 }
+		);
+	}
+
+	let browser;
+	try {
+		browser = await puppeteer.launch();
+		const page = await browser.newPage();
+		await page.goto(url);
+		const screenshot = await page.screenshot({ type: "png" });
+
+		const init = {
+			headers: {
+				"Content-Type": "image/png",
+			},
+		};
+
+		return new Response(screenshot, init);
+	} catch (error) {
+		return NextResponse.json(
+			{ error: "Something went wrong" },
+			{ status: 200 }
+		);
+	} finally {
+		if (browser) {
+			await browser.close();
+		}
+	}
+}
