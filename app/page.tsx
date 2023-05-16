389e14384b583d4ae908d3d7bb0dff15cdd50917
Thabish <kadertabish2@gmail.com>
2023-05-16 14:33:23 +0400

    reasd
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 	return (
 		<main className="flex">
 			<div className="flex items-center">
-				<input type="text" placeholder="Product to be searched" />
+				<input type="text" placeholder="Product to be searched..." />
 				<button>Search</button>
 			</div>
 		</main>
