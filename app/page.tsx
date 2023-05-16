1d9a6b748c10a60660c2c713bc8f87a2d20ba417
Thabish <kadertabish2@gmail.com>
2023-05-16 14:33:12 +0400

    form
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<main className="flex">
-			<div>
+			<div className="flex items-center">
 				<input type="text" placeholder="Product to be searched" />
 				<button>Search</button>
 			</div>
